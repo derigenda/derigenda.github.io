d977ebda063116c276dc3e2c541cb3af8db6bf3b
tpfrobson <t.p.f.robson@cantab.net>
2019-02-18 07:38:50 +0000

    Add files via upload

--- verifier.js
+++ verifier.js
@@ -45,6 +45,43 @@ document.getElementById('verified').style.backgroundColor = 'green'
 
 var str= hexDecode(hexstring)
 var properties = str.split('|')
+
+if(properties[1]=='STAGE1'){properties[1]=('Stage 1')}
+if(properties[1]=='STAGE2'){properties[1]=('Stage 2')}
+if(properties[1]=='STAGE3'){properties[1]=('Stage 3')}
+if(properties[1]=='STAGE4'){properties[1]=('Stage 4')}
+if(properties[1]=='STAGE5'){properties[1]=('Stage 5')}
+if(properties[1]=='STAGE6'){properties[1]=('Stage 6')}
+if(properties[1]=='STAGE7'){properties[1]=('Stage 7')}
+if(properties[1]=='STAGE8'){properties[1]=('Stage 8')}
+if(properties[1]=='STAGE9'){properties[1]=('Stage 9')}
+if(properties[1]=='STAGE10'){properties[1]=('Stage 10')}
+if(properties[1]=='STAGE11'){properties[1]=('Stage 11')}
+if(properties[1]=='STAGE12'){properties[1]=('Stage 12')}
+if(properties[1]=='STAGE13'){properties[1]=('Stage 13')}
+if(properties[1]=='STAGE14'){properties[1]=('Stage 14')}
+if(properties[1]=='STAGE15'){properties[1]=('Stage 15')}
+if(properties[1]=='STAGE16'){properties[1]=('Stage 16')}
+if(properties[1]=='STAGE17'){properties[1]=('Stage 17')}
+if(properties[1]=='STAGE18'){properties[1]=('Stage 18')}
+if(properties[1]=='STAGE19'){properties[1]=('Stage 19')}
+if(properties[1]=='STAGE20'){properties[1]=('Stage 20')}
+if(properties[1]=='OGCSE'){properties[1]=('OCR GCSE')}
+if(properties[1]=='EGCSE'){properties[1]=('Eduqas GCSE')}
+if(properties[1]=='AS'){properties[1]=('AS Level')}
+if(properties[1]=='OVID'){properties[1]=('Additional Ovid')}
+if(properties[1]=='LIVY'){properties[1]=('Additional Livy')}
+if(properties[1]=='CLC1'){properties[1]=('All CLC 1')}
+if(properties[1]=='CLC2'){properties[1]=('All CLC 2')}
+if(properties[1]=='T1'){properties[1]=('Chapter 1')}
+if(properties[1]=='T2'){properties[1]=('Chapter 2')}
+if(properties[1]=='T3'){properties[1]=('Chapter 3')}
+if(properties[1]=='T4'){properties[1]=('Chapter 4')}
+if(properties[1]=='T5'){properties[1]=('Chapter 5')}
+if(properties[1]=='T6'){properties[1]=('Chapter 6')}
+if(properties[1]=='T'){properties[1]=('All Book 1')}
+if(properties[1]=='E'){properties[1]=('OCR GCSE Eng-Lat')}
+
 document.getElementById('initials').innerHTML = properties[0]
 document.getElementById('test').innerHTML = properties[1]
 document.getElementById('range').innerHTML = properties[2]
