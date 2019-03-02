634eaa86e8bc33af2fca26d924bb3c2debd977b0
tpfrobson <t.p.f.robson@cantab.net>
2019-03-02 12:23:23 +0000

    Add files via upload

--- testerscript.js
+++ testerscript.js
@@ -1499,7 +1499,7 @@ document.getElementById('testrange').innerHTML = filter
       function sendemail(){
         
 if (document.getElementById('ininput').value.length>0){
-  document.getElementById('emailbuttons').outerHTML = document.getElementById('emailbuttons').outerHTML + 'Copy link:<br><textarea style="width:100%" onclick="copytext(this)" readonly="readonly" id="theurl2"></textarea>'
+  document.getElementById('emailbuttons').outerHTML = document.getElementById('emailbuttons').outerHTML + 'Copy link:<br><textarea style="width:100%" ondblclick="copytext(this)" onclick="copytext(this)" readonly="readonly" id="theurl2"></textarea>'
  
 var rectw = document.getElementById('EoverLay').getBoundingClientRect().width
 rectw = rectw - 100
@@ -1672,7 +1672,7 @@ function showurl(thebutton){
         'margin-top:-200px';
 
     $('body').append('<div id="Eprompt" style="' + email_overlay + '"><span style="font-size:30px"; id="time">Share this test</span><br><img id="qrcode" width="100" style="cursor:zoom-in" onclick="zoom()"></img><br><h4><ol id="emailbody" style="text-align:left"></ol></h4></div>');
-document.getElementById('emailbody').innerHTML = document.getElementById('emailbody').innerHTML + '<table style="width:90%"><tr><td><button style="width:100%" id="gmailtest" onclick="gmailtest()">Gmail</button></tr></td><tr><td><button style="width:100%" id="emailtest" onclick="emailtest()">Other Email</button></tr></td><tr><td><span style="font-weight: bold;">Copy link:</span><textarea style="width:100%" onclick="copytext(this)" readonly="readonly" id="theurl"></textarea></td></tr><tr><td><button style="width:100%" id="downloadlist" onclick="downloadlist()">Download list</button></td></tr><tr><td><button style="width:100%" id="cancelbutton" onclick="cancelthis()">Close</button></tr></td></table>'
+document.getElementById('emailbody').innerHTML = document.getElementById('emailbody').innerHTML + '<table style="width:90%"><tr><td><button style="width:100%" id="gmailtest" onclick="gmailtest()">Gmail</button></tr></td><tr><td><button style="width:100%" id="emailtest" onclick="emailtest()">Other Email</button></tr></td><tr><td><span style="font-weight: bold;">Copy link:</span><textarea style="width:100%" ondblclick="copytext(this)" onclick="copytext(this)" readonly="readonly" id="theurl"></textarea></td></tr><tr><td><button style="width:100%" id="downloadlist" onclick="downloadlist()">Download list</button></td></tr><tr><td><button style="width:100%" id="cancelbutton" onclick="cancelthis()">Close</button></tr></td></table>'
 document.getElementById('qrcode').setAttribute('src', 'http://api.qrserver.com/v1/create-qr-code/?data=' + encodeURIComponent(window.location.href) + '&size=100x100')
 var create = document.getElementById('downloadlist'),
 textbox = document.getElementById('criteria');
