ba45730c435bc8e95dc5f44266b65dfa1b7846c7
tpfrobson <t.p.f.robson@cantab.net>
2021-06-05 16:00:04 +0100

    Add files via upload

--- testerscript.js
+++ testerscript.js
@@ -688,7 +688,7 @@ function returnlist(q){
      }
 
   var url
- url = '/voc-' + testname + '.htm'
+ url = 'https://derigenda.co.uk/voc-' + testname + '.htm'
 
   var xhr = new XMLHttpRequest();
   // third param = false  = synchronous request
@@ -1740,39 +1740,15 @@ function showurl(thebutton){
         'overflow:auto;' +
         'width:60%;' +
         'text-align:center;' +
-        'height:455px;' +
+        'height:495px;' +
         'margin-left:-30%;' +
         'margin-top:-200px';
 
     $('body').append('<div id="Eprompt" style="' + email_overlay + '"><span style="font-size:30px"; id="time">Share this test</span><br><img id="qrcode" width="100" style="cursor:zoom-in" onclick="zoom()"></img><br><div style="height:32px; font-size:18px;";><div id="classroomText"></div></div><h4><ol id="emailbody" style="text-align:left"></ol></h4></div>');
-document.getElementById('emailbody').innerHTML = document.getElementById('emailbody').innerHTML + '<table  style="width:90%"><tr><td><button style="width:100%" id="gmailtest" onclick="gmailtest()">Gmail</button></td></tr><tr><td><button style="width:100%" id="emailtest" onclick="emailtest()">Other Email</button></td></tr><tr><td><span style="font-weight: bold;">Copy link:</span><textarea style="width:100%" ondblclick="copytext(this)" onclick="copytext(this)" readonly="readonly" id="theurl"></textarea></td></tr><tr><td><button style="width:100%" id="downloadlist" onclick="downloadlist()">Download list</button></td></tr><tr><td><button id="classroomButton" style="width:100%" onclick="classroom()">Share on Google Classroom</button></td></tr><tr><td><button style="width:100%" id="cancelbutton" onclick="cancelthis()">Close</button></td></tr></table>'
+document.getElementById('emailbody').innerHTML = document.getElementById('emailbody').innerHTML + '<table  style="width:90%"><tr><td><button style="width:100%" id="gmailtest" onclick="gmailtest()">Gmail</button></td></tr><tr><td><button style="width:100%" id="emailtest" onclick="emailtest()">Other Email</button></td></tr><tr><td><span style="font-weight: bold;">Copy link:</span><textarea style="width:100%" ondblclick="copytext(this)" onclick="copytext(this)" readonly="readonly" id="theurl"></textarea></td></tr><tr><td><button style="width:100%" id="downloadlist" onclick="listMenu()">Download list</button></td></tr><tr><td><button style="width:100%" id="flashZip" onclick="flashMenu()">Download flashcards</button></td></tr><tr><td><button id="classroomButton" style="width:100%" onclick="classroom()">Share on Google Classroom</button></td></tr><tr><td><button style="width:100%" id="cancelbutton" onclick="cancelthis()">Close</button></td></tr></table>'
 document.getElementById('qrcode').setAttribute('src', 'http://api.qrserver.com/v1/create-qr-code/?data=' + encodeURIComponent(window.location.href) + '&size=100x100')
 var create = document.getElementById('downloadlist'),
 textbox = document.getElementById('criteria');
-
-create.addEventListener('click', function () {
-  if(getParameterByName('filter') == ''){
-alert('Please first select some words.')
-  } else{
-var testname = getParameterByName('test')
-if (testname == 'eg'||testname=='asgrk'||testname=='ggcse'){
-  alert('Greek characters may not be displayed properly.')
-}
-
-var link = document.createElement('a');
-link.setAttribute('download', document.getElementById('menuname').innerText + '.csv');
-link.href = makeTextFile(makecsv());
-document.body.appendChild(link);
-
-// wait for the link to be added to the document
-window.requestAnimationFrame(function () {
-  var event = new MouseEvent('click');
-  link.dispatchEvent(event);
-  document.body.removeChild(link);
-});
-
-}}, false);
-
 
 if (detectmob() == true){
   document.getElementById('gmailtest').outerHTML = ''
@@ -2030,4 +2006,279 @@ if(hn>0){
 timestr = h + ":" + timestr
 }
 return timestr
+}
+
+function FlashZip(){
+  englishWordsString = ''
+  latinWordsString = ''
+  //for each row
+  for (taggy = 0;taggy<document.getElementById('selectionform').getElementsByTagName('tr')[1].childElementCount; taggy++ ){
+     //for each column
+         if(document.getElementById('selectionform').getElementsByTagName('tr')[1].children[taggy].className == 'Meaning-cell' ){
+             var mc = taggy
+           }
+           if(document.getElementById('selectionform').getElementsByTagName('tr')[1].children[taggy].className == 'Latin-cell' ){
+             var lc = taggy
+           }
+       }
+
+  for(i=0;i<document.getElementById('selectionform').getElementsByTagName('tr').length;i++){
+     if(document.getElementById('selectionform').getElementsByTagName('tr')[i].style.backgroundColor=='yellow'){  
+         //if yellow          
+         englishWordsString=englishWordsString + document.getElementById('selectionform').getElementsByTagName('tr')[i].children[mc].innerText + '|'
+         latinWordsString=latinWordsString + document.getElementById('selectionform').getElementsByTagName('tr')[i].children[lc].innerText + '|'
+
+     }}
+     latinWordsString = latinWordsString.substr(0,latinWordsString.length-1)
+     englishWordsString = englishWordsString.substr(0,englishWordsString.length-1)
+ latinWords = latinWordsString.split('|')
+ extras = 4 - (latinWords.length % 4)
+
+ if(extras === 4) {
+     extras = 0
+ }    
+
+ latinWordsString = latinWordsString + '| '.repeat(extras)
+ englishWordsString = englishWordsString + '| '.repeat(extras)
+
+ latinWords = latinWordsString.split('|')
+ englishWords = englishWordsString.split('|')
+
+ rowcount = (latinWords.length)/4
+var finalString = ''
+var sharedStr = '<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="' + englishWords.length + ' uniqueCount="' + englishWords.length + '"><si><t></t>'
+var sheetString = '<worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:xr="http://schemas.microsoft.com/office/spreadsheetml/2014/revision" xmlns:xr2="http://schemas.microsoft.com/office/spreadsheetml/2015/revision2" xmlns:xr3="http://schemas.microsoft.com/office/spreadsheetml/2016/revision3" mc:Ignorable="x14ac xr xr2 xr3" xr:uid="{00000000-0001-0000-0100-000000000000}"><dimension ref="A1:H' + rowcount + '"/><sheetViews><sheetView tabSelected="1" zoomScale="55" zoomScaleNormal="55" workbookViewId="0"/></sheetViews><sheetFormatPr defaultColWidth="8.85546875" defaultRowHeight="34.9" customHeight="1" x14ac:dyDescent="0.2"/><cols><col min="1" max="1" width="22.140625" style="2" customWidth="1"/><col min="2" max="3" width="22.140625" style="3" customWidth="1"/><col min="4" max="4" width="22.140625" style="4" customWidth="1"/><col min="5" max="8" width="22.140625" style="5" customWidth="1"/><col min="9" max="16384" width="8.85546875" style="1"/></cols><sheetData>'
+
+for (i=0;i<rowcount;i++){
+     sheetString = sheetString + '<row r="' + (i+1) + '" spans="1:8" ht="34.9" customHeight="1" x14ac:dyDescent="0.2"><c r="A' + (i+1) + '" s="2" t="str"><v>' + latinWords[i * 4 ].toUpperCase() + '</v></c><c r="B' + (i+1) + '" s="3" t="str"><v>'  + latinWords[i * 4 + 1].toUpperCase() +  '</v></c><c r="C' + (i+1) + '" s="3" t="str"><v>'  + latinWords[i * 4 + 2].toUpperCase() +  '</v></c><c r="D' + (i+1) + '" s="4" t="str"><v>'  + latinWords[i * 4 + 3].toUpperCase() + '</v></c><c r="E' + (i+1) + '" s="5" t="str"><v>' +  englishWords[i * 4 + 3] + '</v></c><c r="F' + (i+1) + '" s="5" t="str"><v>' +  englishWords[i * 4 + 2] +  '</v></c><c r="G' + (i+1) + '" s="5" t="str"><v>'  +  englishWords[i * 4 + 1] +  '</v></c><c r="H' + (i+1) + '" s="5" t="str"><v>'  +  englishWords[i * 4 + 0] +  '</v></c></row>'    
+
+     //finalString = finalString +latinWords[(i * 4) + 0] + '|'
+     //finalString = finalString +latinWords[(i * 4) + 1] + '|'
+     //finalString = finalString +latinWords[(i * 4) + 2] + '|'
+     //finalString = finalString +latinWords[(i * 4) + 3] + '|'
+     //finalString = finalString +englishWords[(i * 4) + 3] + '|'
+     //finalString = finalString +englishWords[(i * 4) + 2] + '|'
+     //finalString = finalString +englishWords[(i * 4) + 1] + '|'
+     //finalString = finalString +englishWords[(i * 4) + 0]
+
+     //if(i!=rowcount) {
+     //    finalString = finalString + "|"
+     //}
+     
+ }
+
+ var portraitOption = $("input:radio[name=orientation]:checked").val()
+
+ if(portraitOption == 'landscape'){
+  sheetString = sheetString + '</sheetData><pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3"/>    <pageSetup paperSize="9" scale="150" fitToWidth="2" fitToHeight="7" pageOrder="overThenDown" orientation="landscape" r:id="rId1"/></worksheet>'  
+ } else {
+  sheetString = sheetString + '</sheetData><pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3"/>    <pageSetup paperSize="9" fitToWidth="2" fitToHeight="7" pageOrder="overThenDown" orientation="portrait" r:id="rId1"/></worksheet>'
+ 
+ }
+
+
+ //sharedStr = sharedStr + finalString.replace(/\|/gm,'\<\/t\>\<\/si\>\<si\>\<t\>')
+ sharedStr = sharedStr + '</t></si></sst>'
+
+ var zip = new JSZip();
+ var url = 'http://derigenda.co.uk/vocabflashcards/docProps/app.xml'
+ zip.file('docProps/app.xml', urlToPromise(url), {binary:true});
+ var url = 'http://derigenda.co.uk/vocabflashcards/docProps/core.xml'
+ zip.file('docProps/core.xml', urlToPromise(url), {binary:true});
+ var url = 'http://derigenda.co.uk/vocabflashcards/xl/printerSettings/printerSettings1.bin'
+ if(portraitOption == 'landscape'){
+  url = 'http://derigenda.co.uk/vocabflashcards/xl/printerSettings/printerSettings1-2.bin'  
+}
+
+ zip.file('xl/printerSettings/printerSettings1.bin', urlToPromise(url), {binary:true});
+ //var url = 'http://derigenda.co.uk/vocabflashcards/xl/sharedStrings.xml'
+ zip.file('xl/sharedStrings.xml', sheetString);
+ var url = 'http://derigenda.co.uk/vocabflashcards/xl/styles.xml'
+ zip.file('xl/styles.xml', urlToPromise(url), {binary:true});
+ var url = 'http://derigenda.co.uk/vocabflashcards/xl/theme/theme1.xml'
+ zip.file('xl/theme/theme1.xml', urlToPromise(url), {binary:true});
+ var url = 'http://derigenda.co.uk/vocabflashcards/xl/workbook.xml'
+ zip.file('xl/workbook.xml', urlToPromise(url), {binary:true});
+ //var url = 'http://derigenda.co.uk/vocabflashcards/xl/worksheets/sheet1.xml'
+ zip.file('xl/worksheets/sheet1.xml', sheetString);
+  var url = 'http://derigenda.co.uk/vocabflashcards/[Content_Types].xml'
+ zip.file('[Content_Types].xml', urlToPromise(url), {binary:true});
+ var url = 'https://derigenda.co.uk/vocabflashcards/sheetrel.xml'
+ zip.file('xl/worksheets/_rels/sheet1.xml.rels', urlToPromise(url), {binary:true});
+ var url = 'https://derigenda.co.uk/vocabflashcards/bookrel.xml'
+ zip.file('xl/_rels/workbook.xml.rels', urlToPromise(url), {binary:true});
+ var url = 'https://derigenda.co.uk/vocabflashcards/rel.xml'
+ zip.file('_rels/.rels', urlToPromise(url), {binary:true});
+ zip.generateAsync({type:"blob"})
+.then(function (blob) {
+ saveAs(blob, "flashcards.xlsx");
+});
+}
+
+function addfile(zip,file){
+ var url = 'http://derigenda.co.uk/' + file
+ zip.file(file, urlToPromise(url), {binary:true});
+}
+
+function urlToPromise(url) {
+ return new Promise(function(resolve, reject) {
+     JSZipUtils.getBinaryContent(url, function (err, data) {
+         if(err) {
+             reject(err);
+         } else {
+             resolve(data);
+         }
+     });
+ });
+}
+
+
+function flashMenu(){
+  var flashM = '<table><tr><td border="10">Size:<br><input type="radio" id="portrait" name="orientation" value="portrait" checked><label for="portrait">Portrait (small)</label><br><input type="radio" id="landscape" name="orientation" value="landscape"><label for="landscape">Landscape (big)</label><br><button value="Generate" style="width:100%" onclick="FlashZip()">Generate</button></td></tr></table>'
+  //document.getElementById('portrait').checked = true
+  document.getElementById('flashZip').outerHTML = document.getElementById('flashZip').outerHTML + '<br>' + flashM
+}
+
+function listMenu(){
+  var flashM = '<table><tr><td border="10">Format:<br><input type="radio" id="xlsx" name="format" value="xlsx" checked><label for="xlsx">Excel</label><br><input type="radio" id="csv" name="format" value="csv"><label for="csv">.csv File</label><br><button value="generate" style="width:100%" onclick="dl()()">Download</button></td></tr></table>'
+  //document.getElementById('portrait').checked = true
+  document.getElementById('downloadlist').outerHTML = document.getElementById('downloadlist').outerHTML + '<br>' + flashM
+}
+
+function dl(){
+  var cOption = $("input:radio[name=format]:checked").val()
+
+ if(cOption == 'csv'){
+  makeCSV()
+ } else {
+  makeXLSX()
+ }
+}
+
+function makeCSV() {
+  if(getParameterByName('filter') == ''){
+alert('Please first select some words.')
+  } else{
+var testname = getParameterByName('test')
+if (testname == 'eg'||testname=='asgrk'||testname=='ggcse'){
+  alert('Greek characters may not be displayed properly.')
+}
+
+var link = document.createElement('a');
+link.setAttribute('download', document.getElementById('menuname').innerText + '.csv');
+link.href = makeTextFile(makecsv());
+document.body.appendChild(link);
+
+// wait for the link to be added to the document
+window.requestAnimationFrame(function () {
+  var event = new MouseEvent('click');
+  link.dispatchEvent(event);
+  document.body.removeChild(link);
+});
+
+}}
+
+
+function makeXLSX(){
+
+headerString = ' '
+firstrow = document.getElementById('selectionform').getElementsByTagName('tr')[0]
+for(j=0;j<firstrow.children.length;j++){
+  headerString = headerString + firstrow.children[j].innerText + '|'
+}  
+headerString = headerString.substr(0,headerString.length-1)
+
+header_s = headerString.split('|')
+
+liststring = ''
+rowstring = ''
+  for(i=0;i<document.getElementById('selectionform').getElementsByTagName('tr').length;i++){
+    if(document.getElementById('selectionform').getElementsByTagName('tr')[i].style.backgroundColor=='yellow'){  
+      rowstring = ''
+      for(k=0;k<header_s.length;k++){
+        rowstring = rowstring + document.getElementById('selectionform').getElementsByTagName('tr')[i].children[k].innerText + '|'
+        
+      }
+      rowstring = rowstring.substr(0,rowstring.length-1)
+      if(rowstring != ''){
+        liststring = liststring + rowstring + '%'
+      }
+    }
+
+  }
+  liststring = liststring.substr(0,liststring.length-1)
+  list = liststring.split('%')
+
+  colcount = header_s.length
+  lastcol = String.fromCharCode(64 + colcount)
+  rowcount = list.length + 1
+  tablestring = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><table xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="xr xr3" xmlns:xr="http://schemas.microsoft.com/office/spreadsheetml/2014/revision" xmlns:xr3="http://schemas.microsoft.com/office/spreadsheetml/2016/revision3" id="1" xr:uid="{00000000-000C-0000-FFFF-FFFF00000000}" name="Table1" displayName="Table1" ref="A1:' + lastcol + rowcount + '" totalsRowShown="0"><autoFilter ref="A1:' + lastcol + rowcount + '" xr:uid="{00000000-0009-0000-0100-000001000000}"/><tableColumns count="' + colcount + '">'
+  
+  //<tableColumn id="1" name=" "/><tableColumn id="2" name="Latin"/><tableColumn id="3" name="Meaning"/></tableColumns><tableStyleInfo name="TableStyleMedium2" showFirstColumn="0" showLastColumn="0" showRowStripes="1" showColumnStripes="0"/></table>'
+  sheet1string = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" mc:Ignorable="x14ac"><dimension ref="A1:' + lastcol + rowcount + '"/><sheetViews><sheetView tabSelected="1" workbookViewId="0"/></sheetViews><sheetFormatPr defaultColWidth="8.85546875" defaultRowHeight="15" x14ac:dyDescent="0.25"/><cols>'
+
+for(m=0;m<colcount;m++){
+  w=0
+  if (m==0){w = 4} else {w = 11}
+
+  uid = '00000' + (m + 1) + '000000'
+  uid = uid.substr(uid.length-12)
+  sheet1string = sheet1string + '<col min="' + (m + 1) + '" max="' + (m + 1) + '" width="' + w + '" bestFit="1" customWidth="1"/>'
+  tablestring = tablestring + '<tableColumn id="' + (m + 1) + '" xr3:uid="{00000000-0009-0000-0100-' + uid + '}" name="' + header_s[m] + '"/>'
+}
+tablestring = tablestring + '</tableColumns><tableStyleInfo name="TableStyleMedium1" showFirstColumn="0" showLastColumn="0" showRowStripes="1" showColumnStripes="0"/></table>'
+sheet1string = sheet1string + '</cols><sheetData><row r="1" spans="1:' + colcount + '" x14ac:dyDescent="0.25">'
+
+tablestring = tablestring.replace(/000001000000\}\" name\=\" \"/g,'000001000000}" name="-"')
+
+for(cc = 0;cc<colcount;cc++){
+  colletter = String.fromCharCode(65 + cc)
+  sheet1string = sheet1string + '<c r="' + colletter + '1' + '" t="str"><v>' + header_s[cc] + '</v></c>'
+}
+sheet1string = sheet1string + '</row>'
+
+for(r=0;r<list.length;r++){
+  sheet1string = sheet1string + '<row r="' + (r + 2) + '" spans="1:' + colcount + '" x14ac:dyDescent="0.25">'
+  for(c = 0;c<colcount;c++){
+    colletter = String.fromCharCode(65 + c)
+    sheet1string = sheet1string + '<c r="' + colletter + (r + 2) + '" t="str"><v>' + list[r].split('|')[c] + '</v></c>'
+  }
+  sheet1string = sheet1string + '</row>'
+}
+
+sheet1string = sheet1string.replace(/\<c r\=\"A1\" t\=\"str\"\>\<v\> \<\/v\>/g,'<c r="A1" t="str"><v>-</v>')
+
+sheet1string = sheet1string + '</sheetData><pageMargins left="0.7" right="0.7" top="0.75" bottom="0.75" header="0.3" footer="0.3"/><tableParts count="1"><tablePart r:id="rId1"/></tableParts></worksheet>'
+  var zip = new JSZip();
+
+  //zip.file('xl/worksheets/sheet1.xml', sheetString);
+  var url = 'https://derigenda.co.uk/spreadsheet/[Content_Types].xml'
+  zip.file('[Content_Types].xml', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/arel.xml'
+  zip.file('_rels/.rels', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/brel.xml'
+  zip.file('xl/_rels/workbook.xml.rels', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/crel.xml'
+  zip.file('xl/worksheets/_rels/sheet1.xml.rels', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/docProps/app.xml'
+  zip.file('docProps/app.xml', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/docProps/core.xml'
+  zip.file('docProps/core.xml', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/xl/sharedStrings.xml'
+  zip.file('xl/sharedStrings.xml', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/xl/styles.xml'
+  zip.file('xl/styles.xml', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/xl/tables/table1.xml'
+  zip.file('xl/tables/table1.xml', tablestring);
+  var url = 'https://derigenda.co.uk/spreadsheet/xl/theme/theme1.xml'
+  zip.file('xl/theme/theme1.xml', urlToPromise(url), {binary:true});
+  var url = 'https://derigenda.co.uk/spreadsheet/xl/workbook.xml'
+  zip.file('xl/workbook.xml', urlToPromise(url), {binary:true});
+  zip.file('xl/worksheets/sheet1.xml', sheet1string);
+
+  zip.generateAsync({type:"blob"})
+ .then(function (blob) {
+  saveAs(blob, "list.xlsx");
+ });
+
 }
